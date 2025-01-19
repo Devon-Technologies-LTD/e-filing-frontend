157d00675fcb58b33909f2e667e2c5c764978ac6
Adewale Adepoju <adewale.adepoju14@gmail.com>
2025-01-19 22:48:23 +0100

    `feat: Refactor navigation and dropdown menu components`

--- components/help-circle.tsx
+++ components/help-circle.tsx
@@ -4,8 +4,6 @@ import {
   DropdownMenuContent,
   DropdownMenuGroup,
   DropdownMenuItem,
-  DropdownMenuLabel,
-  DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
 import Link from "next/link";
@@ -16,33 +14,24 @@ export default function HelpCircleComponent() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <div>
-          <Icons.help  />
+          <Icons.help />
         </div>
       </DropdownMenuTrigger>
       <DropdownMenuContent className="w-56 space-y-3" align="end" forceMount>
-        <DropdownMenuLabel className="font-normal flex items-center gap-3">
-          <div className="flex flex-col space-y-1">
-            <p className="text-sm font-medium leading-none">Jane Doe </p>
-            <p className="text-xs leading-none text-muted-foreground">
-              janedoe@gmail.com{" "}
-            </p>
-          </div>
-        </DropdownMenuLabel>
         <DropdownMenuGroup className="space-y-1">
           <DropdownMenuItem className="uppercase font-semibold text-xs">
-            <Link href="settings/profile">Profile</Link>
+            <Link className="w-full" href="">Read Articles</Link>
           </DropdownMenuItem>
           <DropdownMenuItem className="uppercase font-semibold text-xs">
-            <Link href="settings/security">Account and security</Link>
+            <Link className="w-full" href="">Play tour guide</Link>
           </DropdownMenuItem>{" "}
           <DropdownMenuItem className="uppercase font-semibold text-xs">
-            <Link href="settings/help">Help </Link>
+            <Link className="w-full" href="">Faq </Link>
+          </DropdownMenuItem>
+          <DropdownMenuItem className="uppercase font-semibold text-xs">
+            <Link className="w-full" href="">CONTACT SUPPORT</Link>
           </DropdownMenuItem>
         </DropdownMenuGroup>
-        <DropdownMenuSeparator />
-        <DropdownMenuItem className="uppercase font-semibold text-xs">
-          Log out
-        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
