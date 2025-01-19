ca879a1869bdce945fb111f562018a8655402630
Adewale Adepoju <adewale.adepoju14@gmail.com>
2025-01-19 15:26:17 +0100

    feat:application dashboard layout

---
+++ components/global-search-menu.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+import * as React from "react";
+import { Search } from "lucide-react";
+import { Input } from "./ui/input";
+
+export function SearchMenu() {
+ 
+  return (
+    <>
+      <div className="relative flex-1">
+        <Search className="absolute left-3 top-1/2 -translate-y-1/2 h-4 w-4 text-neutral" />
+        <Input
+          type="search"
+          placeholder="e.g case name, number, magistrate"
+          className="pl-9 border-0 border-b-2 border-border placeholder:text-[#404040] bg-[#F4F4F5] md:w-[100px] lg:w-[350px] opacity-60"
+        />
+      </div>
+    </>
+  );
+}
