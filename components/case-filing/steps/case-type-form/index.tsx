6110494683e090f768acd0867946a7666bff5bfa
Adewale Adepoju <adewale.adepoju14@gmail.com>
2025-01-23 12:09:38 +0100

    `Update case type form component layout to full width`

--- components/case-filing/steps/case-type-form/index.tsx
+++ components/case-filing/steps/case-type-form/index.tsx
@@ -4,7 +4,7 @@ import CaseTypesForm from "./form";
 export default function CaseTypes() {
   return (
     <div className="w-full mr-10 space-y-10 overflow-y-scroll scrollbar-hide h-[calc(100vh-220px)]">
-      <div className="w-[354px] space-y-8">
+      <div className="w-full space-y-8">
         <h2 className="text-sm font-bold text-neutral-500">
           All Fields are required.
         </h2>
