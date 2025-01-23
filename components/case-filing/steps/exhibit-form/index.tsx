5cd65474b90dcadcb6f87b2fc6dd2172006e9719
Adewale Adepoju <adewale.adepoju14@gmail.com>
2025-01-23 04:45:25 +0100

    `Worked on Seal Generation UI workflow`

--- components/case-filing/steps/exhibit-form/index.tsx
+++ components/case-filing/steps/exhibit-form/index.tsx
@@ -3,7 +3,7 @@ import ExhibitFormFields from "./form";
 
 export default function ExhibitForm() {
   return (
-    <div className=" mx-auto space-y-8">
+    <div className="lg:w-1/2 space-y-8">
       <div className="space-y-1">
         <h2 className="text-sm font-bold text-neutral-500">
           Submission of Exhibits are optional but{" "}
