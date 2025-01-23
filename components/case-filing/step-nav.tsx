1e9427d7b23b396d1bfc05eeef633551dabbeb35
Adewale Adepoju <adewale.adepoju14@gmail.com>
2025-01-23 07:21:08 +0100

    `Added CostAssessment component to StepNav component and updated layout`

--- components/case-filing/step-nav.tsx
+++ components/case-filing/step-nav.tsx
@@ -2,16 +2,16 @@
 import { useCaseFilingForm } from "@/context/file-case";
 import { FORM_STEPS, STEP_TITLES } from "@/constants/form";
 import { CaseFilingStepper } from "./case-filing-indicator";
+import CostAssessment from "./cost-assessment";
 
 export function StepNav() {
   const { currentStep } = useCaseFilingForm();
 
   return (
     <div className=" max-h-screen bg-white border-r pr-12">
-      <div className="mx-auto space-y-8">
-        <div className="space-y-2">
+      <div className="mx-auto overflow-auto scrollbar-hide h-[calc(100vh-220px)] space-y-8">
+        <div className="sticky top-0 bg-white z-10 space-y-2">
           <div className="text-xs font-semibold text-gray-600">
-            {" "}
             STEP {currentStep} OF {FORM_STEPS.length}
           </div>
           <div className="text-3xl font-medium leading-8 text-primary">
@@ -20,7 +20,12 @@ export function StepNav() {
         </div>
         <div className="space-y-3 ">
           <CaseFilingStepper steps={FORM_STEPS} currentStep={currentStep} />
-        </div>
+        </div>{" "}
+        {currentStep > 2 && (
+          <div className="space-y-3 ">
+            <CostAssessment />
+          </div>
+        )}
       </div>
     </div>
   );
