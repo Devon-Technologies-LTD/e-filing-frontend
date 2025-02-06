f1545859b2eaa53ef9d1d4d256f3f653e87ec6a3
princemuteh <prince.muteh@gmail.com>
2025-02-06 09:42:49 +0100

    update

--- components/onboarding/onboarding-form.tsx
+++ components/onboarding/onboarding-form.tsx
@@ -1,37 +1,38 @@
-// "use client";
+"use client";
 
-// import { FormStep } from "@/types/file-case";
-// import { useCaseFilingForm } from "@/context/file-case";
-// import { useEffect } from "react";
-// import { LawyerComponent } from "./signup/lawyer";
-// import { OtpComponent } from "./signup/otp";
-// import { IndividualComponent } from "./signup/individual";
+import { FormStep } from "@/types/file-case";
+import { useCaseFilingForm } from "@/context/file-case";
+import { useEffect } from "react";
+import { LawyerComponent } from "./signup/lawyer";
+import { OtpComponent } from "./signup/otp";
+import { IndividualComponent } from "./signup/individual";
 
-// export function OnboardingForm({ initialStep, location }: { initialStep?: number; location?: string }) {
-//   const { currentStep, setCurrentStep } = useCaseFilingForm();
+export function OnboardingForm({ initialStep, location }: { initialStep?: number; location?: string }) {
+  const { currentStep, setCurrentStep } = useCaseFilingForm();
 
-//   useEffect(() => {
-//     if (currentStep !== initialStep) {
-//       setCurrentStep(initialStep as FormStep);
-//     }
-//   }, [initialStep, currentStep, setCurrentStep]);
+  useEffect(() => {
+    if (currentStep !== initialStep) {
+      setCurrentStep(initialStep as FormStep);
+    }
+  }, [initialStep, currentStep, setCurrentStep]);
 
-//   const renderStep = () => {
-//     switch (currentStep) {
-//       case 1:
-//         return <Signup />;
-//       case 2:
-//         if (location === "lawyer") {
-//           return <LawyerComponent />;
-//         } else {
-//           return <IndividualComponent />;
-//         }
-//       case 3:
-//         return <OtpComponent />;
-//       default:
-//         return null;
-//     }
-//   };
+  const renderStep = () => {
+    return <LawyerComponent />;
+    // switch (currentStep) {
+    //   case 1:
+    //     return <Signup />;
+    //   case 2:
+    //     if (location === "lawyer") {
+    //       return <LawyerComponent />;
+    //     } else {
+    //       return <IndividualComponent />;
+    //     }
+    //   case 3:
+    //     return <OtpComponent />;
+    //   default:
+    //     return null;
+    // }
+  };
 
-//   return <div>{renderStep()}</div>;
-// }
+  return <div>{renderStep()}</div>;
+}
