8cafd5a0d5b912349a4bc1cf29fa0eec12325f33
Adewale Adepoju <adewale.adepoju14@gmail.com>
2025-03-24 15:37:14 +0100

    Adds FilePreview component for viewing files

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    env: {
-        NEXT_BASE_URL: process.env.NEXT_BASE_URL,
+  env: {
+    NEXT_BASE_URL: process.env.NEXT_BASE_URL,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.efiling.staging.devontech.io",
       },
+    ],
+  },
 };
 
 export default nextConfig;
--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "api.efiling.staging.devontech.io",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
