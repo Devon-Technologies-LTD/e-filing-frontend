d04f82bffe6690b6f78603a19a3f77b5a43ff930
princemuteh <prince.muteh@gmail.com>
2025-05-14 10:52:24 +0100

    updates

--- components/activity-list.tsx
+++ components/activity-list.tsx
@@ -3,23 +3,6 @@ import React from "react";
 import { Icons } from "./svg/icons";
 import { format } from "date-fns";
 
-// export interface INotification {
-//   id: string;
-//   message: string;
-//   caseNumber: string;
-//   timestamp: string;
-//   initials?: string;
-// }
-
-// export interface Notifications {
-//   id: string;
-//   message: string;
-//   icon: "check" | "scale" | "initials" | "hearings" | "pdf" | "shield";
-//   description: string;
-//   case_id?: string;
-//   status: string;
-//   created_at: string;
-// }
 interface Hearing {
   id: string;
   casefile_id: string;
@@ -31,6 +14,7 @@ interface Hearing {
   created_at: string;
   updated_at: string;
 }
+
 interface ActivityListProps {
   notifications: Hearing[];
   showCaseNumberForCheck?: boolean;
@@ -43,19 +27,28 @@ export function ActivityList({
   return (
     <div className="space-y-6">
       <p className="text-xs font-extrabold text-stone-600">Recent</p>
-      {notifications.map((notification) => (
-        <div key={notification.id} className="border-b py-3">
-          <div className="flex items-center gap-4">
-            <div className="flex space-x-2 w-2/3 items-center">
-              <Icons.calender className="h-1- w-10 flex-shrink-0" />
-              <span className="text-sm font-semibold">{notification.description}</span>
-            </div>
-          </div>
-          <span className="text-xs text-stone-600 font-bold opacity-60 flex justify-end">
-            {format(new Date(notification.created_at ?? ""), "MMM dd, yyyy")}
-          </span>
+
+      {notifications.length === 0 ? (
+        <div className="flex flex-col items-center justify-center text-center text-stone-500 py-10">
+          <Icons.empty className="w-20 h-20 mb-2" />
+          <p className="text-sm font-medium">No recent Hearing</p>
+          <p className="text-xs opacity-70">You're all caught up for now!</p>
         </div>
-      ))}
+      ) : (
+        notifications.map((notification) => (
+          <div key={notification.id} className="border-b py-3">
+            <div className="flex items-center gap-4">
+              <div className="flex space-x-2 w-2/3 items-center">
+                <Icons.calender className="h-1- w-10 flex-shrink-0" />
+                <span className="text-sm font-semibold">{notification.description}</span>
+              </div>
+            </div>
+            <span className="text-xs text-stone-600 font-bold opacity-60 flex justify-end">
+              {format(new Date(notification.created_at ?? ""), "MMM dd, yyyy")}
+            </span>
+          </div>
+        ))
+      )}
     </div>
   );
 }
